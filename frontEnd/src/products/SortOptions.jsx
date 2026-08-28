ae1c8ef5ac5009dac9c6b54b78f0a1a639fbd1f1
Dev-kayes <devkayesmolla@gmail.com>
2026-08-28 16:32:04 +0600

    sort option set to query parameter is done

--- frontEnd/src/products/SortOptions.jsx
+++ frontEnd/src/products/SortOptions.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useSearchParams } from "react-router";
 
 const SortOptions = () => {
-  return <div>SortOptions</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const handleSortChange = (e) => {
+    const sortBy = e.target.value;
+    searchParams.set("sortBy", sortBy);
+    setSearchParams(searchParams);
+  };
+  return (
+    <div className="mb-4 flex justify-end items-center">
+      <select
+        id="sort"
+        onChange={handleSortChange}
+        value={searchParams.get("sortBy")}
+        className="border p-3 rounded-md focus:outline-none"
+      >
+        <option value="">Default</option>
+        <option value="priceAsc">price: Low to High</option>
+        <option value="priceDecs">Price: High to Low</option>
+        <option value="popularity">Popularity</option>
+      </select>
+    </div>
+  );
 };
 
 export default SortOptions;
