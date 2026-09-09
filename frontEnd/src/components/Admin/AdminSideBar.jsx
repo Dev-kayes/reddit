41ef646fdb9c7ad3d8efab16fae237743096a7f7
Dev-kayes <devkayesmolla@gmail.com>
2026-09-09 19:33:27 +0600

    Admin sidebar design is completed

--- frontEnd/src/components/Admin/AdminSideBar.jsx
+++ frontEnd/src/components/Admin/AdminSideBar.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-import { FaUser } from "react-icons/fa";
-import { Link, NavLink } from "react-router";
+import {
+  FaBoxOpen,
+  FaClipboardList,
+  FaSignOutAlt,
+  FaStore,
+  FaUser,
+} from "react-icons/fa";
+import { Link, NavLink, useNavigate } from "react-router";
 
 const AdminSideBar = () => {
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    navigate("/");
+  };
   return (
     <div className="p-6">
       <div className="mb-6">
@@ -31,7 +41,48 @@ const AdminSideBar = () => {
           <FaUser size={24} />
           <span>Users</span>
         </NavLink>
+        <NavLink
+          to="/admin/products"
+          //   className={(isActive) =>
+          //     isActive
+          //      && "bg-red-700 text-white py-3 px-4 rounded flex items-center space-x-2 "
+
+          //   }
+          className={(isActive) =>
+            isActive
+              ? "bg-red-700 text-white py-3 px-4 rounded flex items-center space-x-2 "
+              : "text-gray-300 hover:bg-gray-700 hover:text-white py-3 px-4 rounded flex items-center space-x-2"
+          }
+        >
+          <FaBoxOpen size={24} />
+          <span>Products</span>
+        </NavLink>
+        <NavLink
+          to=""
+          //   className={(isActive) =>
+          //     isActive
+          //      && "bg-red-700 text-white py-3 px-4 rounded flex items-center space-x-2 "
+
+          //   }
+          className={(isActive) =>
+            isActive
+              ? "bg-red-700 text-white py-3 px-4 rounded flex items-center space-x-2 "
+              : "text-gray-300 hover:bg-gray-700 hover:text-white py-3 px-4 rounded flex items-center space-x-2"
+          }
+        >
+          <FaStore size={24} />
+          <span>Shop</span>
+        </NavLink>
       </nav>
+      <div className="mt-6">
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white flex space-x-2 items-center py-3 px-4 rounded w-full"
+          onClick={handleLogOut}
+        >
+          <FaSignOutAlt />
+          <span>Logout</span>
+        </button>
+      </div>
     </div>
   );
 };
